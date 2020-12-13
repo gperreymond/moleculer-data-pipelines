e1ecae74a87d06d8a880bedcce31b8f258ae77dd
Gilles Perreymond <gperreymond@gmail.com>
2020-12-13 09:22:15 +0100

    feat(): argocd setup

--- README.md
+++ README.md
@@ -6,9 +6,11 @@
 # setup
 $ curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
 $ chmod +x ./kind
-# after created
-$ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 ```
 
 * https://kind.sigs.k8s.io/docs/user/quick-start/
 * https://kind.sigs.k8s.io/docs/user/ingress/#ingress-nginx
+
+### Argocd
+
+* Start with disable auth (not production ready of course)
--- start.sh
+++ start.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
 ./kind create cluster --name infra --config cluster.yaml
+
 kubectl cluster-info --context kind-infra
+
+sleep 2
+
+# nginx
+kubectl apply -f _charts/nginx.yaml
+
+echo "Waiting for ingress to setup"
+sleep 60
+echo "Ingress is ok, continue..."
+
+# argocd
+kubectl create namespace moleculer
+kubectl create namespace argocd
+kubectl apply -n argocd -f _charts/argocd.yaml
+kubectl apply -n argocd -f _charts/argocd-ingress.yaml
