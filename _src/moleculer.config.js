f73ae1c20c89c90a6fe7ebfba9e9c6e0b318dab4
Gilles Perreymond <gperreymond@gmail.com>
2020-12-16 23:56:35 +0100

    feat(k8s): exposed tcp done

--- _src/moleculer.config.js
+++ _src/moleculer.config.js
@@ -1,16 +1,17 @@
 const { v4: uuidv4 } = require('uuid')
 
 const { name, version } = require('./package.json')
-const { nats } = require('./application.config')
+const { rabbitmq } = require('./application.config')
 
 module.exports = {
   nodeID: `node-${name}-${version}-${uuidv4()}`,
   transporter: {
-    type: 'NATS',
+    type: 'AMQP',
     options: {
-      url: `nats://${nats.hostname}:${nats.port}`,
-      user: nats.username,
-      pass: nats.password
+      url: `amqp://${rabbitmq.username}:${rabbitmq.password}@${rabbitmq.hostname}:${rabbitmq.port}`,
+      eventTimeToLive: 5000,
+      prefetch: 1,
+      autoDeleteQueues: true
     }
   },
   logger: true,
